ba556d86aa475dfdce43c20e6e0c038b801d2098
Vova <100944929+VladimirFedulov@users.noreply.github.com>
2026-01-12 23:02:33 +0300

    Create migration script to merge heads
    This migration script merges multiple heads into a single revision.

---
+++ app/db/migrations/versions/app/db/migrations/versions/9f2a3c4d5e6f_merge_heads.py
@@ -0,0 +1,37 @@
+"""merge heads
+
+Revision ID: 9f2a3c4d5e6f
+Revises: 2b231de97dc3, 305943d779c4, 54c4b8c525fc, 5a4446e7b165, 852d951c9c08, 8f1b5f45f2f9, a0d3d400ea75, adda2dd4a741, b3378dc6de01, d02dcfbf1517, d0a3960f5dad, fad8b1997c3a, fe7796f840a4
+Create Date: 2025-02-14 00:00:00.000000
+
+"""
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "9f2a3c4d5e6f"
+down_revision = (
+    "2b231de97dc3",
+    "305943d779c4",
+    "54c4b8c525fc",
+    "5a4446e7b165",
+    "852d951c9c08",
+    "8f1b5f45f2f9",
+    "a0d3d400ea75",
+    "adda2dd4a741",
+    "b3378dc6de01",
+    "d02dcfbf1517",
+    "d0a3960f5dad",
+    "fad8b1997c3a",
+    "fe7796f840a4",
+)
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
