6d98ec62e16b26fb68c5a533ca264c386aa28add
Vova <100944929+VladimirFedulov@users.noreply.github.com>
2026-01-25 17:57:36 +0300

    Update down_revision identifier in migration file

--- app/db/migrations/versions/0b6ef93e6a2c_add_subscription_types_to_hosts.py
+++ app/db/migrations/versions/0b6ef93e6a2c_add_subscription_types_to_hosts.py
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "0b6ef93e6a2c"
-down_revision = "c1f6e2b6b2f4"
+down_revision = "1f2a0b7d9a8c"
 branch_labels = None
 depends_on = None
 
